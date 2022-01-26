da06c7e7b1caa1b8b142944d85403bb0694840af
Supr0en <erik.ruotsalainen1@gmail.com>
2022-01-26 09:31:14 +0200

    04-task muutoksia

--- 04-task/src/komponentit/Form.js
+++ 04-task/src/komponentit/Form.js
@@ -4,22 +4,23 @@ import View from './View';
 const Form = () => {
   return (
     <section className="Form">
+    <h1>Fill the form</h1>
       <form>
         <div className="mb-3">
-            <label for="EtuNimi" className="form-label">Etunimi: </label>
-            <input className="form-control" aria-describedby="EtuNimi" id="EtuNimi" type="text" />
+            <label for="eNimi" className="form-label">Etunimi: </label>
+            <input className="form-control" aria-describedby="eNimi" id="eNimi" type="text" />
         </div>
         <div className="mb-3">
-            <label for="EtuNimi" className="form-label">Sukunimi: </label>
-            <input className="form-control" aria-describedby="EtuNimi" id="EtuNimi" type="text" />
+            <label for="sNimi" className="form-label">Sukunimi: </label>
+            <input className="form-control" aria-describedby="sNimi" id="sNimi" type="text" />
         </div>
         <div className="mb-3">
-            <label for="EtuNimi" className="form-label">Puhelin: </label>
-            <input className="form-control" aria-describedby="EtuNimi" id="EtuNimi" type="text" />
+            <label for="puh" className="form-label">Puhelin: </label>
+            <input className="form-control" aria-describedby="puh" id="puh" type="text" />
         </div>
         <div className="mb-3">
-            <label for="EtuNimi" className="form-label">Message: </label>
-            <input className="form-control" aria-describedby="EtuNimi" id="EtuNimi" type="text" />
+            <label for="msg" className="form-label">Message: </label>
+            <input className="form-control" aria-describedby="msg" id="msg" type="text" />
         </div>
       </form>
       <View />
