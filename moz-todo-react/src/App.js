605740f529ceb41d6b380dd05e364835d988691c
Supr0en <erik.ruotsalainen1@gmail.com>
2022-02-02 12:47:19 +0200

    kesken todo-tetava

---
+++ moz-todo-react/src/App.js
@@ -0,0 +1,64 @@
+import './App.css';
+import Todo from "./components/Todo.js";
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+
+const App = (props) => {
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const toggleTaskCompleted = (id) => {
+    const updatedTasks = tasks.map(task => {
+      if(id === task.id) {
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  const deleteTask = (id) => {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  const taskList = tasks.map(task => (
+      <Todo id={task.id} 
+        name={task.name} 
+        completed={task.completed} 
+        key={task.id} 
+        toggleTaskCompleted={toggleTaskCompleted} 
+        deleteTask={deleteTask} 
+      />
+    ));
+
+  const addTask = (name) => {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} tasks remaining`;
+
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+      {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
