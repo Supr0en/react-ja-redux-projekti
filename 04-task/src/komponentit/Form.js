45f10cd2026aa17f2be6a6537c94907acf3c97a7
Supr0en <erik.ruotsalainen1@gmail.com>
2022-02-08 12:59:37 +0200

    04-task tehty loppuun

--- 04-task/src/komponentit/Form.js
+++ 04-task/src/komponentit/Form.js
@@ -1,29 +1,38 @@
 import './Form.css';
 import View from './View';
+import React, {useState} from 'react';
 
 const Form = () => {
+
+  const [eNimi, seteNimi] = useState();
+  const [sNimi, setsNimi] = useState();
+  const [puh, setpuh] = useState();
+  const [msg, setmsg] = useState();
   return (
     <section className="Form">
-    <h1>Fill the form</h1>
+      <h2>Fill the form</h2>
       <form>
         <div className="mb-3">
             <label for="eNimi" className="form-label">Etunimi: </label>
-            <input className="form-control" aria-describedby="eNimi" id="eNimi" type="text" />
+            <input className="form-control" aria-describedby="eNimi" id="eNimi" type="text" onChange={(e) => seteNimi(e.target.value)} />
         </div>
         <div className="mb-3">
             <label for="sNimi" className="form-label">Sukunimi: </label>
-            <input className="form-control" aria-describedby="sNimi" id="sNimi" type="text" />
+            <input className="form-control" aria-describedby="sNimi" id="sNimi" type="text" onChange={(e) => setsNimi(e.target.value)} />
         </div>
         <div className="mb-3">
             <label for="puh" className="form-label">Puhelin: </label>
-            <input className="form-control" aria-describedby="puh" id="puh" type="text" />
+            <input className="form-control" aria-describedby="puh" id="puh" type="text" onChange={(e) => setpuh(e.target.value)} />
         </div>
         <div className="mb-3">
             <label for="msg" className="form-label">Message: </label>
-            <input className="form-control" aria-describedby="msg" id="msg" type="text" />
+            <input className="form-control" aria-describedby="msg" id="msg" type="text" onChange={(e) => setmsg(e.target.value)} />
         </div>
+
+
       </form>
-      <View />
+      
+      <View eNimi={eNimi} sNimi={sNimi} puh={puh} msg={msg} />
     </section>
   );
 }
--- 04-task/src/komponentit/View.js
+++ 04-task/src/komponentit/View.js
@@ -1,27 +1,26 @@
+import React, {useState} from "react";
 import './View.css';
+import Form from "./Form";
 
-const [title, setTittle] = useState('')
+const View = (props) => {
+  
+  const [title, setTittle] = useState('')
 
-const View = () => {
   return (
     <div className="View">
-      <h1>This is input</h1>
+      <h2>This is your input:</h2>
       <form>
         <div className="mb-3">
-            <label for="eNimi" className="form-label">Etunimi: </label>
-            <input onChange={event => setTittle(event.target.value)} />
+          <p><span className="form-label">Etunimi: </span>{props.eNimi}</p>
         </div>
         <div className="mb-3">
-            <label for="sNimi" className="form-label">Sukunimi: </label>
-            <input onChange={event => setTittle(event.target.value)} />
+          <p><span className="form-label">Sukunimi: </span>{props.sNimi}</p>
         </div>
         <div className="mb-3">
-            <label for="puh" className="form-label">Puhelin: </label>
-            <input onChange={event => setTittle(event.target.value)} />
+          <p><span className="form-label">Puhelin: </span>{props.puh}</p>
         </div>
         <div className="mb-3">
-            <label for="msg" className="form-label">Message: </label>
-            <input onChange={event => setTittle(event.target.value)} />
+          <p><span className="form-label">Message: </span>{props.msg}</p>
         </div>
       </form>
     </div>
