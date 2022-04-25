09134c2373f24032750af1285bf79a640623f9eb
Supr0en <erik.ruotsalainen1@gmail.com>
2022-04-25 14:19:16 +0300

    notes valmis

---
+++ notes/src/index.js
@@ -0,0 +1,31 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+const notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.0982",
+    important: true
+
+  },
+  {
+    id: 2,
+    content: "Browser con execure only JavaScript",
+    date: "2019-05-30T18:39:34.0912",
+    important: false
+
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methdods of HTTP protocol",
+    date: "2019-05-30T19:20:14.2982",
+    important: true
+
+  }
+]
+ReactDOM.render(
+  <App notes={notes} />,
+  document.getElementById( 'root' )
+)
